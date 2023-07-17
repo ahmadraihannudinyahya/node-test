7a63ec0eb76ad9157e895efaf0f2ff88570b2628
yahya <ahmad.raihannudin.yahya@gmail.com>
2023-07-17 13:57:11 +0700

    test var

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+var param;
+
+app.get('/var', (_, res) => {
+  res.send({result : param});
+});
+
+app.get('/var/:value', (req, res) => {
+  const { value } = req.params;
+  param = value;
+  res.send({result : value});
+});
+
 app.get('/test', (_, res) => {
   console.log('test');
   res.send({result : "test"});
